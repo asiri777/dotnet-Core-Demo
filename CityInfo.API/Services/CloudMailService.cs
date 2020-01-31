5bb8603861e979623ed69deef451ef982f374e8a
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-31 21:20:28 +0530

    Change appsettings

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -8,8 +8,8 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@test.com";
-        private string _mailFrom = "noreply@test.com";
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
 
         public void Send(string subject, string message)
         {
